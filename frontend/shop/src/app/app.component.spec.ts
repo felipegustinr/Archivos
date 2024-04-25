5c4a0fb2572d1d0f20d70f234256692b08d7cddd
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-04-25 17:07:23 -0500

    Create Project

--- frontend/shop/src/app/app.component.spec.ts
+++ frontend/shop/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'shoping' title`, () => {
+  it(`should have the 'shop' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('shoping');
+    expect(app.title).toEqual('shop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, shoping');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, shop');
   });
 });
