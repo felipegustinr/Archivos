e178b3617a1ab02a0bc68240b3c70ac9ffcc9e72
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-04-25 00:13:52 -0500

    Create items.js

---
+++ backend/shoped/models/items.js
@@ -0,0 +1,68 @@
+const {
+  DataTypes
+} = require('sequelize');
+module.exports = sequelize => {
+  const attributes = {
+    id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      defaultValue: null,
+      comment: null,
+      primaryKey: true,
+      field: "id",
+      autoIncrement: true
+    },
+    name: {
+      type: name,
+      allowNull: false,
+      defaultValue: null,
+      comment: null,
+      primaryKey: false,
+      field: "name",
+      autoIncrement: false
+    },
+    category: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: null,
+      comment: null,
+      primaryKey: false,
+      field: "category",
+      autoIncrement: false
+    },
+    date_add: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      defaultValue: null,
+      comment: null,
+      primaryKey: false,
+      field: "date_add",
+      autoIncrement: false
+    },
+    in_stock: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: null,
+      comment: null,
+      primaryKey: false,
+      field: "in_stock",
+      autoIncrement: false
+    },
+    price: {
+      type: money,
+      allowNull: false,
+      defaultValue: null,
+      comment: null,
+      primaryKey: false,
+      field: "price",
+      autoIncrement: false
+    }
+  };
+  const options = {
+    tableName: "items",
+    comment: "",
+    indexes: []
+  };
+  const ItemsModel = sequelize.define("items_model", attributes, options);
+  return ItemsModel;
+};
