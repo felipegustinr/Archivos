31610ceb5be65cb99b6ebbbc95c2c797bb6c9773
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-04-25 01:15:54 -0500

    Update items.js

--- backend/shoped/routes/items.js
+++ backend/shoped/routes/items.js
@@ -2,4 +2,8 @@ var express = require('express');
 var router = express.Router();
 const itemsController = require('../controllers').itemsController;
 router.get('/', itemsController.list);
+router.get('/:id', itemsController.getById);
+router.post('/', itemsController.add);
+router.put('/:id', itemsController.update);
+router.delete('/:id', itemsController.delete);
 module.exports = router;
