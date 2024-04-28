be06d9b828f2e413a2045e4b8dd81cf79db002bb
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-04-28 14:46:25 -0500

    Update app.routes.ts

--- frontend/shop/src/app/app.routes.ts
+++ frontend/shop/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ItemsComponent } from './components/items/items.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home', component: HomeComponent
+    },
+    {
+        path: 'items', component: ItemsComponent
+    }
+];
