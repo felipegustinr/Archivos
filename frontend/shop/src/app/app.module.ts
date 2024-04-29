39daa15c5048d4bedff1071484bd994f1b24fe11
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-04-29 11:55:22 -0500

    Update

---
+++ frontend/shop/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ItemsComponent } from './components/items/items.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ItemsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
