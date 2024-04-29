33fb1772634a5a504259e1bbf85f5ebfa82ba216
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-04-29 14:40:35 -0500

    Create items.service.ts

---
+++ frontend/shop/src/app/services/items/items.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemsService {
+
+  constructor(
+    protected http: HttpClient
+    ) { }
+    getItems(): Observable<any> {
+      let route = [environment.apiUrl, 'items'].join('/');
+      return this.http.get(route);
+      }
+}
