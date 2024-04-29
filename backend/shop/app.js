b1ba7aadf404208e51ac7f7050c4311c2b18e30d
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-04-29 01:43:38 -0500

    Update app.js

--- backend/shop/app.js
+++ backend/shop/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var itemsRouter = require('./routes/items');
+var cors = require('cors');
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/items', itemsRouter);
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
