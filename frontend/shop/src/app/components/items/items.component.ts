543a5df25e33b6a9deb0ebb00cfce4c39ba97444
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-04-29 14:40:10 -0500

    Update items.component.ts

--- frontend/shop/src/app/components/items/items.component.ts
+++ frontend/shop/src/app/components/items/items.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { ItemsService } from '../../services/items/items.service';
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
-  styleUrl: './items.component.scss'
+  styleUrls: ['./items.component.scss']
 })
-export class ItemsComponent {
+export class ItemsComponent implements OnInit {
+  items: any;
 
+  constructor(
+    private router: Router,
+    private itemsService: ItemsService
+  ) { }
+
+  ngOnInit(): void {
+    let Items = this.itemsService.getItems().subscribe(
+      {
+        next: (data: any) => { 
+          this.items = data;
+          console.log(data);
+        },
+        error: (err: any) => { 
+          console.error(err);
+        }
+      }
+    );
+  }
 }
