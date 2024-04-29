5244c8013ff11fdccb3ee5eb77ab2867822150ff
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-04-29 12:29:24 -0500

    Update app-routing.module.ts

--- frontend/shop/src/app/app-routing.module.ts
+++ frontend/shop/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ItemsComponent } from './components/items/items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'items', component: ItemsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
