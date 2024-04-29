168a7241ee3013bbd7b2d11040fba5a1ae50026a
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-04-29 12:29:38 -0500

    Update app.module.ts

--- frontend/shop/src/app/app.module.ts
+++ frontend/shop/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ItemsComponent } from './components/items/items.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ItemsComponent } from './components/items/items.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
