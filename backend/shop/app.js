d509b73a13f1ea5bd3d7e5092542d9f54e23ac77
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-04-29 14:43:59 -0500

    Update app.js

--- backend/shop/app.js
+++ backend/shop/app.js
@@ -11,10 +11,10 @@ var itemsRouter = require('./routes/items');
 
 var app = express();
 
-// Habilitar CORS para todas las rutas
+
 app.use(cors());
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -24,19 +24,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Definir las rutas de la API después de habilitar CORS
+
 app.use('/items', itemsRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
